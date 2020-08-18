fd7d49b8ed695ccd2192e45184e832c6aa3c3215
hamadialvin <hamadialvin@yahoo.com>
2020-08-18 13:15:09 +0300

    Enabling Js

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -8,10 +8,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView mywebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mywebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings= mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("");
     }
 }
