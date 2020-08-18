360e3138204da3c151dcd287b5afa05d6f5f15a9
hamadialvin <hamadialvin@yahoo.com>
2020-08-18 13:10:30 +0300

    Import web view settings

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.webviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
